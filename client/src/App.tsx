362f51fadda82d0da6bdf2c554f6586f6b430547
sundayucheawaji <sunday@smartaccessgroup.co.uk>
2026-02-17 18:46:37 +0100

    invoke component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
-import ReviewList from './components/reviews/ReviewList';
+// import ChatBot from "./components/chat/ChatBot";
+import ReviewList from "./components/reviews/ReviewList";
 
 function App() {
-   return (
-      <div className="p-4 h-screen">
-         <ReviewList productId={5} />
-      </div>
-   );
+  return (
+    <div className="p-4 h-screen">
+      {/* review summarizer */}
+      <ReviewList productId={5} /> 
+      
+      {/* Interactive chat for an imaginary Park called WonderWorld */}
+      {/* <ChatBot /> */}
+    </div>
+  );
 }
 
 export default App;
